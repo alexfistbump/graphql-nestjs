5c78132ebdd734405c3ca2466889e4ba3288c669
Alejandro Gonzalez <alex@fistbump.co>
2022-06-14 01:20:00 +0200

    pets module/resolver/service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 import { PetsModule } from './pets/pets.module';
 
 @Module({
-  imports: [PetsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      debug: false,
+      playground: true,
+      autoSchemaFile: join('src/schema.gql')
+    }),
+    PetsModule,
+  ],
 })
 export class AppModule {}
